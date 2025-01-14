0bd0fefa5f839a878f2a0b1fa9216756a06bfef3
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2025-01-14 16:40:48 +0100

    📦 updates version

--- public/apps/app.asset.php
+++ public/apps/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '7108e8ce8f3a2cb88b1e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'f0132065ad996bfecace');
