cc802d9f56694b59d2938ff349ef497724251f64
Giovambattista Fazioli <gfazioli@users.noreply.github.com>
2026-04-20 09:50:53 +0200

    🚀 chore(release): bump to v2.0.1 (#6)
    Pulls in the WPBones v2.0.1 inline-scripts fix.
    Version aligned to 2.0.1 across readme.txt, wp-kirk.php, package.json.

--- wp-kirk.php
+++ wp-kirk.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP Kirk Mantine Boilerplate
  * Plugin URI: https://github.com/wpbones/WPKirk-Mantine-Boilerplate
  * Description: WP Bones template WordPress plugin
- * Version: 2.0.0
+ * Version: 2.0.1
  * Requires at least: 6.2
  * Requires PHP: 7.4
  * Author: Giovambattista Fazioli
